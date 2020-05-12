3d0152d689995891d228487e951492db7eacebf6
a.kuznecoff82@gmail.com <a.kuznecoff82@gmail.com>
2020-05-12 14:02:23 +0300

    my commit

---
+++ LinqApp_Task2/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqApp_Level2
+{
+    class Actor
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+
+    abstract class ArtObject
+    {
+
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+    class Film : ArtObject
+    {
+
+        public int Length { get; set; }
+        public IEnumerable<Actor> Actors { get; set; }
+    }
+    class Book : ArtObject
+    {
+
+        public int Pages { get; set; }
+    }
+    class Program
+    {
+    
+        static List<object>Data()
+        {
+            var data = new List<object>() {
+                        "Hello",
+                        new Book() { Author = "Terry Pratchett", Name = "Guards! Guards!", Pages = 810 },
+                        new List<int>() {4, 6, 8, 2},
+                        new string[] {"Hello inside array"},
+                        new Film() { Author = "Martin Scorsese", Name= "The Departed", Actors = new List<Actor>() {
+                            new Actor() { Name = "Jack Nickolson", Birthdate = new DateTime(1937, 4, 22)},
+                            new Actor() { Name = "Leonardo DiCaprio", Birthdate = new DateTime(1974, 11, 11)},
+                            new Actor() { Name = "Matt Damon", Birthdate = new DateTime(1970, 8, 10)}
+                        }},
+                        new Film() { Author = "Gus Van Sant", Name = "Good Will Hunting", Actors = new List<Actor>() {
+                            new Actor() { Name = "Matt Damon", Birthdate = new DateTime(1970, 8, 10)},
+                            new Actor() { Name = "Robin Williams", Birthdate = new DateTime(1951, 8, 11)},
+                        }},
+                        new Book() { Author = "Stephen King", Name="Finders Keepers", Pages = 200},
+                        "Leonardo DiCaprio"
+                    };
+            return data;
+        }
+        static void Main(string[] args)
+        {
+           
+
+            Console.ReadLine();
+        }
+
+        static void Task1()
+        {
+            var data = Data();
+            Console.WriteLine("\nTask 1 :  Output all elements excepting ArtObjects");
+            Console.WriteLine(string.Join(",", data.Except(data.OfType<ArtObject>())));
+        }
+
+        static void Task2()
+        {
+            var data = Data();
+            Console.WriteLine("\nTask 2 : Output all actors names\n");
+            Console.WriteLine(string.Join("\n", data.OfType<Film>()
+                             .SelectMany(film => film.Actors,(film, actor)=> actor.Name)
+                             .Distinct()));
+        }
+               
+        static void Task3()
+        {
+            var data = Data();
+            Console.WriteLine("\nTask 3 : Output number of actors born in august\n");
+            Console.WriteLine(data.OfType<Film>()
+                             .SelectMany(film => film.Actors
+                             .Where(a => a.Birthdate.Month == 8))
+                             .GroupBy(a => a.Name, birthday => birthday.Birthdate)
+                             .Count());
+                           
+        }
+
+        static void Task4()
+        {
+            Console.WriteLine("\nTask 4 : Output two oldest actors names\n");
+            Console.WriteLine(string.Join("\n", Data().OfType<Film>()
+                           .SelectMany(film => film.Actors).OrderBy(actor =>actor.Birthdate.Year)
+                           .Select(actor => actor.Name).Take(2)));
+        }
+
+        static void Task5()
+        {
+           Console.WriteLine("\nTask 5 : Output number of books per authors\n");
+            Console.WriteLine(string.Join("\n",Data().OfType<Book>()
+                            .GroupBy(book => book.Author)
+                            .Select(gr => $"{gr.Key} : {gr.Count()}")));
+        }
+
+        static void Task6()
+        {
+            Console.WriteLine("\nTask 6 : Output number of books per authors and films per director\n");
+            Console.WriteLine(string.Join("\n",Data().OfType<ArtObject>()
+                             .GroupBy(o => o.Author)
+                             .Select(gr => $"{gr.Key} : {gr.Count()}")));
+        }
+
+        static void Task7()
+        {
+            Console.WriteLine("\nTask 7 : Output how many different letters used in all actors names\n");
+            Console.WriteLine(Data().OfType<Film>()
+                            .SelectMany(film => film.Actors,(film,actor) =>actor.Name)
+                            .Distinct()
+                            .SelectMany(name =>name).Where(ch => ch != ' ')
+                            .Select(litera => litera)
+                            .Count());
+        }
+
+        static void Task8()
+        {
+            Console.WriteLine("\nTask 8 : Output names of all books ordered by names of their authors and number of pages\n");
+            Console.WriteLine(string.Join("\n", Data().OfType<Book>()
+                              .OrderBy(book => book.Author).ThenBy(book => book.Pages)
+                              .Select(b => $"{b.Author} : {b.Pages}")));
+                            
+        } 
+
+        static void Task9()
+        {
+            var data = Data();
+
+            Console.WriteLine("T\nask 9 : Output actor name and all films with this actor\n");
+            Console.WriteLine(string.Join("\n", data.OfType<Film>()
+                             .SelectMany(film => film.Actors)
+                             .GroupBy(actor => actor.Name, actor => data.OfType<Film>()
+                             .Where(film => film.Actors.Contains(actor))
+                             .Select(film => film.Name))
+                             .Select(gr => $" Actor : {gr.Key} - {string.Join("\n\t", gr.SelectMany(film => film))}")));
+         
+        }
+
+        static void Task10()
+        {
+            Console.WriteLine("\nTask 10. Output sum of total number of pages in all books and all int values inside all sequences in data\n");
+            Console.WriteLine(Data().OfType<Book>().Select(book => book.Pages).Sum()
+                               +Data().OfType<IEnumerable<int>>().Sum(sum => sum.Sum()));
+        }
+
+        static void Task11()
+        {
+            Console.WriteLine("\nTask 11. Get the dictionary with the key - book author, value - list of author's books\n");
+            Console.WriteLine(string.Join("\n",Data().OfType<Book>()
+                              .ToDictionary(book => book.Author)
+                               .Select(dict => $"{dict.Key}  - {dict.Value.Name}")));
+        }
+
+        static void Task12()
+        {
+            var data = Data();
+            Console.WriteLine("\nTask 12 : Output all films of Matt Damon excluding films with actors whose name are presented in data as strings\n");
+            Console.WriteLine(string.Join("\n",data.OfType<Film>()
+                              .Where(film => film.Actors.Any(actor => actor.Name =="Matt Damon")
+                              &&film.Actors.All(actor => !data.OfType<string>()
+                              .Contains(actor.Name)))
+                              .Select(film => film.Name)));
+        }
+    }
+}
---
+++ LinqApp_Tasks1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqApp_Level1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     
+        
+
+            Console.ReadKey();
+        }
+
+        static void Task1()
+        {
+            Console.WriteLine("\nTask 1 : Print all numbers from 10 to 50 separated by commas");
+            Console.WriteLine(string.Join(",",Enumerable.Range(10,50)));
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine("\nTask 2 : Print only that numbers from 10 to 50 that can be divided by 3");
+            Console.WriteLine(string.Join(",", Enumerable.Range(10, 50).Where(i => i % 3 == 0)));
+        }
+
+        static void Task3()
+        {
+            Console.WriteLine("\nTask 3 : Output word Linq 10 times");
+            Console.WriteLine(string.Join(",",Enumerable.Repeat("Linq",10)));
+        }
+
+        static void Task4()
+        {
+            Console.WriteLine("\nTask 4 : Output all words with letter 'a' in string aaa; abb; ccc; dap");
+            Console.WriteLine(string.Join(",", "aaa; abb; ccc; dap".Split(';')
+                             .Where(i => i.Contains('a'))));
+        }
+
+        static void Task5()
+        {
+            Console.WriteLine("\nTask 5 : Output number of letters 'a' in the words with this letter in string  aaa; abb; ccc; dap separated by comma");
+            Console.WriteLine(string.Join(",", "aaa; abb; ccc; dap".Split(';')
+                              .Where(i => i.Contains('a'))
+                               .Select(i =>i.Count(litera => litera=='a'))));
+        }
+
+        static void Task6()
+        {
+            Console.WriteLine("Task 6 : Output true if word abb exists in line  aaa; xabbx; abb; ccc; dap, otherwise false");
+            Console.WriteLine(string.Join(",", "aaa; xabbx; abb; ccc; dap".Split(';')
+                              .Select(i => i.Contains("abb"))));
+        }
+
+        static void Task7()
+        {
+            Console.WriteLine("\nTask 7 :  Get the longest word in string aaa; xabbx; abb; ccc; dap");
+            Console.WriteLine("aaa; xabbx; abb; ccc; dap".Split(';')
+                              .First(str => "aaa; xabbx; abb; ccc; dap".Split(';')
+                               .Max(s => s.Length)==str.Length));
+        }
+
+        static void Task8()
+        {
+            Console.WriteLine("\nTask 8 : Calculate average length of word in string aaa; xabbx; abb; ccc; dap");
+            Console.WriteLine(" aaa; xabbx; abb; ccc; dap".Split(';')
+                             .Average(str => str.Length));
+        }
+
+        static void Task9()
+        {
+            Console.WriteLine("\nTask 9 : Print the shortest word reversed in string ak; xabbx; abb; ccc; dap; zha ");
+            Console.WriteLine("ak; xabbx; abb; ccc; dap; zha".Split(';')
+                             .Last(str => "ak; xabbx; abb; ccc; dap; zh".Split(';')
+                              .Min(s => s.Length)==str.Length)
+                              .Reverse().ToArray());
+        }
+
+        static void Task10()
+        {
+            Console.WriteLine("\nTask 10 : " +
+                "            Print true if in the first word that starts from aa all letters are 'a' " +
+                "            otherwise false baaa; aabb; xabbx; abb; ccc; dap; zh");
+            Console.WriteLine("baaa; aabb; xabbx; abb; ccc; dap; zh".Split(';')
+                             .FirstOrDefault(str=> str.StartsWith("aa"))
+                              .Any(ch => ch=='a'));
+        }
+
+        static void Task11()
+        {
+            Console.WriteLine("\nTask 11 :  Print last word in sequence that excepting first two elements that ends with bb");
+            Console.WriteLine("baaa; aabb; xabbx; abb; ccc; dap; zh".Split(';')
+                             .SkipWhile((str,index) => str.EndsWith("bb")&& index <2)
+                             .Last());
+        }
+
+
+    }
+}
